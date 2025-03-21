21fcb9eefa0d279c881368882fc4e102e25f3f58
Sean Murphy <sean@iamseanmurphy.com>
2025-03-21 18:18:12 +0000

    1.7.0

--- woocommerce-payfast-gateway/woocommerce-gateway-payfast.php
+++ woocommerce-payfast-gateway/woocommerce-gateway-payfast.php
@@ -6,11 +6,11 @@
  * Description: Receive payments using the South African Payfast payments provider.
  * Author: WooCommerce
  * Author URI: https://woocommerce.com/
- * Version: 1.6.10
- * Requires at least: 6.5
+ * Version: 1.7.0
+ * Requires at least: 6.6
  * Tested up to: 6.7
- * WC requires at least: 9.4
- * WC tested up to: 9.6
+ * WC requires at least: 9.5
+ * WC tested up to: 9.7
  * Requires PHP: 7.4
  * PHP tested up to: 8.3
  *
@@ -19,7 +19,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'WC_GATEWAY_PAYFAST_VERSION', '1.6.10' ); // WRCS: DEFINED_VERSION.
+define( 'WC_GATEWAY_PAYFAST_VERSION', '1.7.0' ); // WRCS: DEFINED_VERSION.
 define( 'WC_GATEWAY_PAYFAST_URL', untrailingslashit( plugins_url( '/', __FILE__ ) ) );
 define( 'WC_GATEWAY_PAYFAST_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 
