35c3d1122c50fa991f7e7db6800f45661e2d4d5f
Sean Murphy <sean@iamseanmurphy.com>
2025-08-11 21:17:46 +0000

    1.7.3

--- woocommerce-payfast-gateway/build/payment-method.asset.php
+++ woocommerce-payfast-gateway/build/payment-method.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => 'b0111b6015aca3a444a9');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => '488c79fc361b8fc4e9aa');
